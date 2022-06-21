ad769e39a966b8721bf73ee1ed82dc6bb2e1e77f
Dhananjay Jindal <68967358+dhananjayjindal@users.noreply.github.com>
2022-06-21 11:40:27 +0530

    Time: 168 ms (58.36%), Space: 61.1 MB (91.01%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -6,7 +6,10 @@ public:
         sort (nums.begin(),nums.end());
         for (int i=0;i<n;i++){
             if (nums[i]==nums[i+1]){
-            return nums[i];}}
+            return nums[i];}
+            else if (nums[n-i-1]==nums[n-2-i]){
+                return nums[n-i-1];
+            }}
         return 0;
     }
 };
