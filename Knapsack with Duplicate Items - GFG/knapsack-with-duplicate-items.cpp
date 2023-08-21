51f5a31858f0a35e7e345ca26768c20b967cf65b
Dhananjay Jindal <68967358+dhananjayjindal@users.noreply.github.com>
2023-08-21 23:06:07 +0530

    Added solution - LeetHub

--- Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.cpp
+++ Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.cpp
@@ -8,24 +8,37 @@ using namespace std;
 // User function Template for C++
 
 class Solution{
-    private:
+    // private:
     
-    int f(int ind , int mw , int val[], int wt[] , vector<vector<int>>&dp){
-        if (ind ==0) return (mw/wt[0])*val[0];
-        if (dp[ind][mw]!=-1) return dp[ind][mw];
-        int not_take = 0 + f(ind-1, mw , val , wt , dp);
-        int take = INT_MIN;
-        if (wt[ind]<=mw)
-        take = val[ind] + f(ind, mw - wt[ind] , val , wt , dp );
-        return dp[ind][mw] =  max (take,not_take);
-    }
+    // int f(int ind , int mw , int val[], int wt[] , vector<vector<int>>&dp){
+    //     if (ind ==0) return (mw/wt[0])*val[0];
+    //     if (dp[ind][mw]!=-1) return dp[ind][mw];
+    //     int not_take = 0 + f(ind-1, mw , val , wt , dp);
+    //     int take = INT_MIN;
+    //     if (wt[ind]<=mw)
+    //     take = val[ind] + f(ind, mw - wt[ind] , val , wt , dp );
+    //     return dp[ind][mw] =  max (take,not_take);
+    // }
     
 public:
     int knapSack(int N, int W, int val[], int wt[])
     {
         // code here
-        vector<vector<int>>dp(N,vector<int>(W+1,-1));
-        return f(N-1,W,val,wt,dp);
+        vector<vector<int>>dp(N,vector<int>(W+1,0));
+        for (int w = 0 ; w<=W ; w++ ){
+            dp[0][w] = (w/wt[0])*val[0];
+        }
+        
+        for(int ind = 1 ; ind <N ; ind++){
+            for(int w = 0 ; w<=W ; w++){
+                int not_take = 0 + dp[ind-1][w];
+                int take = INT_MIN;
+                if (wt[ind]<=w)
+                take = val[ind] + dp[ind][w - wt[ind]];
+                dp[ind][w] = max(take,not_take);
+            }
+        }
+        return dp[N-1][W];
     }
 };
 
